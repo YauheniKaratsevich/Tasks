5631add80f48f47f5ec2766baa9b8e484f88d7f5
Yauheni UL20A <evgen.korotsevich@gmail.com>
2018-03-27 21:43:56 +0300

    Sting was changed

--- T1.2/minshweather.sh
+++ T1.2/minshweather.sh
@@ -4,7 +4,7 @@ while [ 1 ]
 do
 	var=$(cat config.ini | sed 's/period=//g')
 	wget -q pogoda.tut.by
-	echo -n "В Минске сейчас "
+	echo -n "Temperature in Minsk: "
 	cat index.html | grep temp-i | sed 's/<span class="temp-i">//g;s/&deg;<\/span><\/div>//g' | sed '1!d'
 	rm index.html
 	sleep $var
