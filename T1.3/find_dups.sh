f8f5fa27c7f5eaa4492c097c907343c12c319251
Yauheni Karatsevich <evgen.korotsevich@gmail.com>
2018-03-29 23:27:10 +0300

    Part name is done

--- T1.3/find_dups.sh
+++ T1.3/find_dups.sh
@@ -1,1 +1,25 @@
 #!/bin/bash
+
+# for entry in *
+# do
+# 	echo "$entry"
+# done
+
+# find . -type f
+
+# find * -type f -printf '%f\n' | sort | uniq -dc
+# find . -type f -printf '%p %f\n'
+IFS=$'\n'
+f=`find . -type f -printf '%p\n'`
+echo "#!/bin/bash" >> log.txt
+for file1 in $f
+do
+	for file2 in $f
+	do
+		if [ $(basename "$file1") == $(basename "$file2") ]; then		
+			echo "####------------------| $(basename "$file1") |------------------" >> log.txt
+			echo "#rm $file1" >> log.txt
+			echo "#rm $file2" >> log.txt
+		fi
+	done
+done
