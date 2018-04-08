2283ce7ffc1f68196740e14a6db3a4527b8b8867
Yauheni Karatsevich <evgen.korotsevich@gmail.com>
2018-04-08 15:38:41 +0300

    Bug fix in 'by_cont' feature

--- T1.3/find_dups.sh
+++ T1.3/find_dups.sh
@@ -12,14 +12,14 @@ else
 fi
 
 IFS=$'\n'
-# f=`find . -type f -printf '%p\n'`
-f=`find . -type f | rev | sort | rev`
 script_file="rm_dups.sh"
 echo "#!/bin/bash" > $script_file
 #-----------------------name-----------------------------------------------
 if [ $mode == "by_name" ]; then
 	echo "##### BY NAME ######" >> $script_file
 
+	f=`find . -type f | rev | sort | rev`
+	
 	#remove unique file names
 	result="uniq"
 	for file1 in $f
@@ -53,26 +53,59 @@ if [ $mode == "by_name" ]; then
 		previous_file_name=${current_file_name}
 	done
 
-	echo $f
 	exit 1
-
 fi
 
 #-----------------------cont-----------------------------------------------
 if [ $mode == "by_cont" ]; then
 	echo "#####  DONE BY CONTENT ##### " >> $script_file
+
+	f=`find . -type f -exec md5sum {} \; | sort`
+
+	#remove unique md5
+	result="uniq"
 	for file1 in $f
 	do
+		current_file1_md5=${file1:0:34}
+		current_file1_name=${file1/${current_file1_md5}}
+
 		for file2 in $f
 		do
-			if [ "$file1" != "$file2" ]; then	#not file itself
-				if cmp -s "$file1" "$file2"; then		
-					printf "%s\n" "####------------------| $(basename "$file1") |------------------" >> $script_file
-					printf "#rm %q\n" "$file1" >> $script_file
-					printf "#rm %q\n" "$file2" >> $script_file
+			current_file2_md5=${file2:0:34}
+			current_file2_name=${file2/${current_file2_md5}}	
+
+			if [ "$current_file1_name" != "$current_file2_name" ]; then	#not file itself
+				if [ "$current_file1_md5" == "$current_file2_md5" ]; then	
+					result="not_uniq"
+					break
 				fi
 			fi
 		done
+
+		if [[ "$result" == "uniq" ]]; then
+			f=${f/$file1}
+		fi
+
+		result="uniq"
 	done
+
+	#output in a file
+	previous_file_md5="0"
+	previous_file_name="/"
+	for current_string in $f
+	do
+		current_file_md5=${current_string:0:34}
+		current_file_name=${current_string/${current_file_md5}}
+
+		if [ "$previous_file_md5" != "$current_file_md5" ]; then	
+			printf "%s\n" "####------------------| MD5: $current_file_md5 |------------------" >> $script_file
+		fi	
+
+		printf "#rm %q\n" "$current_file_name" >> $script_file
+		
+		previous_file_md5=${current_file_md5}
+		previous_file_name=${current_file_name}
+	done
+
 	exit 1
 fi
