a0a21dfe130d4d5de2eeeddaace690365cc797bf
Yauheni UL20A <evgen.korotsevich@gmail.com>
2018-04-24 21:53:44 +0300

    T1.4 added

---
+++ T1.4/c.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main( void ) {
+	while( 1 ) {
+		printf("C:\n");
+	}
+	return 0;
+}
---
+++ T1.4/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main( int argc, char *argv[] ) {
+	pid_t pid = fork();
+	switch (pid) {
+	
+	}
+	return 0;
+}
